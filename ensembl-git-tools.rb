94fd1e72d1f134f7c37531a8e5eea3c6f22c4b46
Andrew Yates <ayates@ebi.ac.uk>
2016-06-16 12:15:24 +0100

    New formula for Ensembl Git Tools.

--- ensembl-git-tools.rb
+++ ensembl-git-tools.rb
@@ -1,12 +1,15 @@
 class EnsemblGitTools < Formula
   desc "Library of extended Git functions for working with Ensembl"
   homepage "https://github.com/ensembl/ensembl-git-tools"
-  # tag "versioncontrol"
 
-  url "https://github.com/Ensembl/ensembl-git-tools/archive/1.0.5.tar.gz"
+  url "https://github.com/Ensembl/ensembl-git-tools/archive/1.0.6.tar.gz"
+  sha256 "9f2d1859a0b9ad6889aaa505518170969d433c09dd1dece5a02f5ce177a9aaef"
 
   def install
-    bin.install "git-ensembl"
+    bin.install Dir["bin/*"]
+    prefix.install "advanced_bin/"
+    lib.install "lib/EnsEMBL"
+    prefix.install "hooks"
   end
 
   test do
