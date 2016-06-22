af30c5c39d9ff972ed343051cda96ef04e975f6a
Andrew Yates <ayates@ebi.ac.uk>
2016-06-22 14:02:54 +0100

    bwa now compiles

--- bwa-051mt.rb
+++ bwa-051mt.rb
@@ -10,9 +10,9 @@ class Bwa051mt < Formula
   keg_only 'bwa 0.5.1-mt creates binaries with the same name as all other bwa installs'
   
   def install
-    system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}"
+    inreplace 'Makefile', '-g -Wall -O2', '-std=gnu89 -g -Wall -O2'
+    system "make"
     bin.install "bwa"
-    doc.install "README.md", "NEWS.md"
     man1.install "bwa.1"
   end
 
@@ -20,6 +20,8 @@ class Bwa051mt < Formula
     (testpath/"test.fasta").write ">0\nAGATGTGCTG\n"
     system "#{bin}/bwa", "index", "test.fasta"
     assert File.exist?("test.fasta.bwt")
-    assert_match "AGATGTGCTG", shell_output("#{bin}/bwa mem test.fasta test.fasta")
+    system "#{bin}/bwa", "aln", "-f", "test.bwa", "test.fasta", "test.fasta"
+    system "#{bin}/bwa", "samse", "-f", "test.sam", "test.fasta", "test.bwa", "test.fasta"
+    assert File.exist?("test.sam");
   end
 end
