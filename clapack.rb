0f1b79765d4ba493df4e3508323c85a84c5439e0
Andrew Yates <ayates@ebi.ac.uk>
2016-06-23 17:40:07 +0100

    Supporting phast with clapack

--- clapack.rb
+++ clapack.rb
@@ -2,7 +2,7 @@ class Clapack < Formula
   desc 'Algebra functions'
   homepage 'https://github.com/CshlSiepelLab/phast'
   url 'http://www.netlib.org/clapack/clapack-3.2.1.tgz'
-  sha256 ''
+  sha256 '6dc4c382164beec8aaed8fd2acc36ad24232c406eda6db462bd4c41d5e455fac'
   version '3.2.1'
   
   keg_only 'Conflicts with openblas'
@@ -12,5 +12,9 @@ class Clapack < Formula
     system 'make', 'f2clib'
     system 'make', 'blaslib'
     system 'make', 'lib'
+
+    (prefix+'F2CLIBS').install 'F2CLIBS/libf2c.a'
+    prefix.install 'INCLUDE'
+    prefix.install Dir['*.a']
   end
 end
--- phast.rb
+++ phast.rb
@@ -2,13 +2,19 @@ class Phast < Formula
   desc ' Phylogenetic analysis with space/time models'
   homepage 'https://github.com/CshlSiepelLab/phast'
   url 'https://github.com/CshlSiepelLab/phast/archive/v1.3.tar.gz'
-  sha256 'f936897d80fba814ca30ed7671f4448ee1e5fde2f697ebf0d5ced74bf46e4972'
+  sha256 'db4e58b694274b08a5265630eb8c0620e84d10a49149929dddd5ef6f65924b2f'
   version '1.3'
   
-  depends_on :lapack
+  depends_on 'ensembl/ensembl/clapack'
   
   def install
-    lapack = Formula[:lapack].opt_prefix
-    system 'make', 'install', "DESTDIR=#{prefix}", "CLAPACKPATH=#{lapack}"
+    lapack = Formula['ensembl/ensembl/clapack'].opt_prefix
+    inreplace 'src/Makefile', '${PWD}', "#{buildpath}/src"
+    cd 'src' do
+      system 'make', "CLAPACKPATH=#{lapack}", "PHAST=#{buildpath}"
+    end
+    bin.install Dir['bin/*']
+    lib.install 'lib/libphast.a'
+    prefix.install 'data'
   end
 end
