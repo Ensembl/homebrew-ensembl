8c43d320fd7699667dd8445f230b81046267a3ce
Andrew Yates <andrewyatz@users.noreply.github.com>
2016-06-23 15:39:35 +0100

    Update sha256

--- parsimonator.rb
+++ parsimonator.rb
@@ -2,7 +2,7 @@ class Parsimonator < Formula
   desc 'A fast parsimony program for building phylogenies from DNA'
   homepage 'http://sco.h-its.org/exelixis/web/software/parsimonator/index.html'
   url 'https://github.com/stamatak/Parsimonator-1.0.2/archive/master.zip'
-  sha256 '1f5be1c5b5c703c0b03969af7d8fa85da280044f131757c76b66e9021b67ae18'
+  sha256 'da935321313fdd9b5d85cbdf2720329a6bff4d831f08646f32d1e0c3d65e254a'
   version '1.0.2'
 
   def install
