64c6a7cd68fa115ef04e1fa295f9e9efa82d446c
Andrew Yates <ayates@ebi.ac.uk>
2016-06-24 14:19:03 +0100

    Adding prank. Copy from homebrew science with edits

---
+++ prank.rb
@@ -0,0 +1,22 @@
+class Prank < Formula
+  desc "A multiple alignment program for DNA, codon and amino-acid sequences"
+  homepage "http://wasabiapp.org/software/prank/"
+  url "http://wasabiapp.org/download/prank/prank.source.140603.tgz"
+  sha256 "9a48064132c01b6dba1eec90279172bf6c13d96b3f1b8dd18297b1a53d17dec6"
+
+  depends_on "mafft"
+  depends_on "ensembl/ensembl/exonerate22"
+
+  def install
+    cd "src" do
+      system "make"
+      bin.install "prank"
+      man1.install "prank.1"
+    end
+  end
+
+  test do
+    system "#{bin}/prank", "-help"
+  end
+end
+
