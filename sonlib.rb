f920dac05c561619b869334f6d08ce045158a5ad
Andrew Yates <ayates@ebi.ac.uk>
2016-06-27 12:05:00 +0100

    Adding sonLib

---
+++ sonlib.rb
@@ -0,0 +1,11 @@
+class Sonlib < Formula
+  desc 'Small general purpose library for C and Python with focus on bioinformatics'
+  homepage 'https://github.com/benedictpaten/sonLib'
+  url 'https://github.com/benedictpaten/sonLib.git', :using => :git
+  version '0.1'
+
+  def install
+    system 'make', 'all'
+    libexec.install Dir['*']
+  end
+end
