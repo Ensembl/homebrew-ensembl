cb555af6ba59709ad3c0cff7208b41a2594ffd7b
Andrew Yates <ayates@ebi.ac.uk>
2016-06-27 11:50:05 +0100

    Adding pecan

---
+++ pecan.rb
@@ -0,0 +1,13 @@
+class Pecan < Formula
+  desc 'Probabilistic consistency based aligner'
+  homepage 'http://hgwdev.cse.ucsc.edu/~benedict/code/Pecan.html'
+  url 'https://github.com/benedictpaten/pecan.git', :using => :git
+  version '0.9.0'
+
+  def install
+    system 'javac', 'bp/pecan/Pecan.java'
+    system 'javac', 'bp/trawler/Trawler.java'
+    libexec.install 'bp'
+    doc.install Dir['doc/*']
+  end
+end
