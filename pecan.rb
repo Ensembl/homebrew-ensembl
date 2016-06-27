835ef95454a775e4a8debd4d81a902abf826fd5c
Andrew Yates <ayates@ebi.ac.uk>
2016-06-27 12:52:42 +0100

    Getting the version right

--- pecan.rb
+++ pecan.rb
@@ -2,7 +2,7 @@ class Pecan < Formula
   desc 'Probabilistic consistency based aligner'
   homepage 'http://hgwdev.cse.ucsc.edu/~benedict/code/Pecan.html'
   url 'https://github.com/benedictpaten/pecan.git', :using => :git
-  version '0.9.0'
+  version '0.8.0'
 
   def install
     system 'javac', 'bp/pecan/Pecan.java'
