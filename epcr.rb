5dabbc80434e00d2ae92b688025e0d4c829cf012
Andrew Yates <ayates@ebi.ac.uk>
2016-06-28 13:23:41 +0100

    Fixed build by stopping linuxbrew from cloberring

--- epcr.rb
+++ epcr.rb
@@ -7,10 +7,15 @@ class Epcr < Formula
   url "ftp://ftp.ncbi.nlm.nih.gov/pub/schuler/e-PCR/e-PCR-2.3.12-1-src.tar.gz"
   sha256 "92613a09cbba3eab66916488063b56e2a3b50a82e5308b1731b6b90d232b8275"
 
+  env :std
+
   def install
+    ENV.deparallelize
+    ENV.delete('CFLAGS')
+    ENV.delete('CXXFLAGS')
+    ENV.delete('LDFLAGS')
     system 'make links depend all OPTIMIZE=6'
-    bin.install [%e-PRC re-PRC famap fahash%]
-    lib.install "libepcr.a"
+    bin.install %w(e-PCR re-PCR famap fahash)
   end
 
   test do
