598f79aa9c5b5ca3b1e008ea160c11ab5aed01ac
Andrew Yates <ayates@ebi.ac.uk>
2016-06-28 14:16:26 +0100

    Making fasttree default to double

---
+++ fasttree.rb
@@ -0,0 +1,27 @@
+class Fasttree < Formula
+  homepage "http://microbesonline.org/fasttree/"
+  # doi "10.1371/journal.pone.0009490"
+  # tag "bioinformatics"
+
+  url "http://microbesonline.org/fasttree/FastTree-2.1.8.c"
+  sha256 "b172d160f1b12b764d21a6937c3ce01ba42fa8743d95e083e031c6947762f837"
+
+  def install
+    opts = %w[-O3 -finline-functions -funroll-loops]
+    opts << "-DUSE_DOUBLE" 
+    system ENV.cc, "-o", "FastTree", "FastTree-#{version}.c", "-lm", *opts
+    bin.install "FastTree"
+  end
+
+  test do
+    (testpath/"test.fa").write <<-EOF.undent
+      >1
+      LCLYTHIGRNIYYGSYLYSETWNTTTMLLLITMATAFMGYVLPWGQMSFWGATVITNLFSAIPYIGTNLV
+      >2
+      LCLYTHIGRNIYYGSYLYSETWNTGIMLLLITMATAFMGYVLPWGQMSFWGATVITNLFSAIPYIGTNLV
+      >3
+      LCLYTHIGRNIYYGSYLYSETWNTGIMLLLITMATAFMGTTLPWGQMSFWGATVITNLFSAIPYIGTNLV
+    EOF
+    assert_match(/1:0.\d+,2:0.\d+,3:0.\d+/, `#{bin}/FastTree test.fa`)
+  end
+end
