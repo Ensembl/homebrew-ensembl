adc04e21fd9cddf7e0ba59dbe7922fa9a2ee2a15
Andrew Yates <andrewyatz@users.noreply.github.com>
2016-08-03 15:29:43 +0100

    Adding phobius

--- interproscan.rb
+++ interproscan.rb
@@ -5,8 +5,9 @@ class Interproscan < Formula
   url 'ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/5.19-58.0/interproscan-5.19-58.0-64-bit.tar.gz'
   sha256 'aca23a9461536f81b27081c0df70c7813fe20cfba19f084cf5ab45202c0eef84'
 
-  depends_on 'tmhmm'
-  depends_on 'signalp'
+  depends_on 'ensembl/moonshine/tmhmm'
+  depends_on 'ensembl/moonshine/signalp'
+  depends_on 'ensembl/moonshine/phobius'
 
   resource 'panther-models' do
     url 'ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/data/panther-data-10.0.tar.gz'
@@ -14,8 +15,9 @@ class Interproscan < Formula
   end
 
   def install
-    tmhmm = Formula['tmhmm']
-    signalp = Formula['signalp']
+    tmhmm = Formula['ensembl/moonshine/tmhmm']
+    signalp = Formula['ensembl/moonshine/signalp']
+    phobius = Formula['ensembl/moonshine/phobius']
 
     inreplace 'interproscan.properties' do |s|
       # Handle tmhmm config
@@ -25,6 +27,9 @@ class Interproscan < Formula
       # Handle signalp dependencies
       s.gsub! 'bin/signalp/4.1/signalp', "#{signalp.bin}/signalp"
       s.gsub! 'bin/signalp/4.1/lib', "#{signalp.lib}"
+      
+      # Handle phobius dependencies
+      s.gsub! 'bin/phobius/1.01/phobius.pl', "#{phobious.prefix}/phobius.pl"
     end
 
     prefix.install Dir['*']
