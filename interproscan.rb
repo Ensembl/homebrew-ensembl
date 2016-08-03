2e35ccc3ea5f603925f18f1a27b9b6b5680754f8
Andrew Yates <andrewyatz@users.noreply.github.com>
2016-08-03 12:01:23 +0100

    Adding in code to make tmhmm and signalp available

--- interproscan.rb
+++ interproscan.rb
@@ -5,12 +5,28 @@ class Interproscan < Formula
   url 'ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/5.19-58.0/interproscan-5.19-58.0-64-bit.tar.gz'
   sha256 'aca23a9461536f81b27081c0df70c7813fe20cfba19f084cf5ab45202c0eef84'
 
+  depends_on 'tmhmm'
+  depends_on 'signalp'
+
   resource 'panther-models' do
     url 'ftp://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/data/panther-data-10.0.tar.gz'
     sha256 '231581055bd038312d67fa1adc90c721cd286b7c571f7b3314ee531bac4b9764'
   end
 
   def install
+    tmhmm = Formula['tmhmm']
+    signalp = Formula['signalp']
+
+    inreplace 'interproscan.properties' do |s|
+      # Handle tmhmm config
+      s.gsub! 'bin/tmhmm/2.0c/decodeanhmm', "#{tmhmm.bin}/decodeanhmm.Linux_x86_64"
+      s.gsub! 'data/tmhmm/2.0c/TMHMM2.0c.model', "#{tmhmm.lib}"
+
+      # Handle signalp dependencies
+      s.gsub! 'bin/signalp/4.1/signalp', "#{signalp.bin}/signalp"
+      s.gsub! 'bin/signalp/4.1/lib', "#{signalp.lib}"
+    end
+
     prefix.install Dir['*']
     resource('panther-models').stage do
       (prefix+'data'+'panther').mkdir
