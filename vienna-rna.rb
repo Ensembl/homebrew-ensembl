a5d1dd13133973a703c29ca315a7f468b234dc20
Thibaut Hourlier <thibaut@ebi.ac.uk>
2016-10-31 16:59:26 +0000

    Added revision to make sure it will recompile

--- vienna-rna.rb
+++ vienna-rna.rb
@@ -14,6 +14,7 @@ class ViennaRna < Formula
   url 'http://www.tbi.univie.ac.at/RNA/download/sourcecode/2_2_x/ViennaRNA-2.2.6.tar.gz', :using => IgnoreRedirectionsDownloadStrategy
   sha256 '7bd2d385ef7496ed54fd0576c6664cd88b93543d30932a13a2e3354d9ccdb530'
   version '2.2.6'
+  revision 1
   
   def install
     system './configure', "--prefix=#{prefix}", "--without-doc-pdf"
