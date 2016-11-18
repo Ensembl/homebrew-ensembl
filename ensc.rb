57dbb52f201631b5c489e98580961e0707cf9883
Andrew Yates <andrewyatz@users.noreply.github.com>
2016-11-18 10:46:58 +0000

    Switch version to latest
    Formula pointed to master which was a moving target. Solution is to path directly to a hash on GitHub. Version is the 6 character start of the hash. Will investigate moving to specific targeted versions in time.

--- ensc.rb
+++ ensc.rb
@@ -2,9 +2,9 @@ class Ensc < Formula
   desc 'C bindings to Ensembl DBs and useful utils'
   homepage 'https://github.com/Ensembl/ensc-core'
 
-  url 'https://github.com/Ensembl/ensc-core/archive/master.zip'
-  sha256 '388575886468805da8f335ab7cb8a42bd55924eb7db81ea838ca3057c049960d'
-  version '1.1.0'
+  url 'https://github.com/Ensembl/ensc-core/archive/8a0feab034441203e5f81e9f6cfa27b9e24d9252.zip'
+  sha256 '7d099851bc4975bf45c2e1d59af36acb216e0dd4fb390928ddb4e662abe94dfa'
+  version '8a0feab'
 
   depends_on "ensembl/ensembl/mysql-client"
   depends_on "ensembl/ensembl/htslib" => :recommended
