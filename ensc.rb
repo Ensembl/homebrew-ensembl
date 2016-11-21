e92c6dc143af09798fa7ceee557c18e36f839930
Andrew Yates <andrewyatz@users.noreply.github.com>
2016-11-21 09:54:22 +0000

    Now at v1.0.0

--- ensc.rb
+++ ensc.rb
@@ -2,9 +2,9 @@ class Ensc < Formula
   desc 'C bindings to Ensembl DBs and useful utils'
   homepage 'https://github.com/Ensembl/ensc-core'
 
-  url 'https://github.com/Ensembl/ensc-core/archive/8a0feab034441203e5f81e9f6cfa27b9e24d9252.zip'
-  sha256 '7d099851bc4975bf45c2e1d59af36acb216e0dd4fb390928ddb4e662abe94dfa'
-  version '8a0feab'
+  url 'https://github.com/Ensembl/ensc-core/archive/1.0.0.zip'
+  sha256 'b05e9bc5950d07cd410b4eebf6354bb9ed3c8387afd97043109225a3db09a813'
+  version '1.0.0'
 
   depends_on "ensembl/ensembl/mysql-client"
   depends_on "ensembl/ensembl/htslib" => :recommended
