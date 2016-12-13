76e896b48d8b2bbae082626eca875a26538b761e
Alessandro Vullo <avullo@ebi.ac.uk>
2016-12-13 14:41:13 +0000

    Expect camel case?

--- bioperl-run-169.rb
+++ bioperl-run-169.rb
@@ -9,7 +9,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-class Bioperlrun169 < Formula
+class BioperlRun169 < Formula
   homepage 'http://bioperl.org/'
   desc 'Perl library for bioinformatic applications'
   url 'https://github.com/bioperl/bioperl-run/archive/release-1-6-9.tar.gz'
