a182fb5f312249c8b923b2a160bcb9e90c9f1c20
Thibaut Hourlier <thibaut@ebi.ac.uk>
2017-02-08 13:36:59 +0000

    Renamed file to avoid problems in brew and now use the correct version

--- infernal10.rb
+++ infernal10.rb
@@ -9,17 +9,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-class Infernal < Formula
+class Infernal10 < Formula
   desc "Search DNA databases for RNA structure and sequence similarities"
   homepage "http://eddylab.org/infernal/"
   # doi "10.1093/bioinformatics/btp157"
   # tag "bioinformatics"
 
-  version "0.55"
-  url "http://eddylab.org/software/infernal/infernal-0.55.tar.gz"
-  sha256 "935a21b14b9fa1cc91e8c46a12443d19d0b1cd8a8f5a83c5aa878fab5ba87feb"
+  version "1.0"
+  url "http://eddylab.org/software/infernal/infernal-#{version}.tar.gz"
+  sha256 "099d322c1aa1434f9a37379375f24e02cf4eceb102b12690ba1c65958a73999f"
 
-  keg_only "Genebuild uses version 0.55"
+  keg_only "Genebuild uses version #{version}"
 
   def install
     system "./configure", "--prefix=#{prefix}"
