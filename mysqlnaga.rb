00c33dc256c05f29d027a8ead6b9e152e474e427
Dan Staines <dstaines@ebi.ac.uk>
2017-08-08 17:20:00 +0100

    updated to v1.01

--- mysqlnaga.rb
+++ mysqlnaga.rb
@@ -12,9 +12,9 @@
 class Mysqlnaga  < Formula
   desc 'mysqlnaga database copying utility'
   homepage 'https://github.com/EnsemblGenomes/mysqlnaga'
-  url 'https://github.com/EnsemblGenomes/mysqlnaga/archive/v1.0.tar.gz'
-  sha256 '83a3585cb7d875fe62e740295e64ecdd15c7c81faea387cba4fd91ae090f86a4'
-  version '1.0'
+  url 'https://github.com/EnsemblGenomes/mysqlnaga/archive/v1.01.tar.gz'
+  sha256 'fcc96aab2c4c0e46a7bcfd674be8877894491eaf11ce0235efb475d5962b847d'
+  version '1.01'
 
   def install
     bin.install Dir["bin/*"]
