13a2a53933ba2f322b271b5ad8875f10dbe42c15
Andrew Yates <ayates@ebi.ac.uk>
2017-08-14 16:14:18 +0100

    Adding dependency on 1.8 HDF5

--- hal.rb
+++ hal.rb
@@ -12,17 +12,22 @@
 class Hal < Formula
   desc 'Hierarchical Alignment Format library'
   homepage 'https://github.com/ComparativeGenomicsToolkit/hal'
-  url 'https://github.com/ComparativeGenomicsToolkit/hal.git', :using => :git
-  version '451788f'
+  url 'https://github.com/ComparativeGenomicsToolkit/hal.git', :using => :git, :revision => '1a89bd2'
+  version '1a89bd2'
 
   depends_on 'ensembl/ensembl/sonlib'
-  depends_on 'hdf5'
+  depends_on 'hdf5@1.8'
 
-  keg_only 'Use directly because of large numbers of installed bianries and headers'
+  keg_only 'Use directly because of large numbers of installed binaries and headers'
 
   def install
     ENV.deparallelize
+    
     sonlib = Formula['ensembl/ensembl/sonlib']
+    hdf5 = Formula['hdf5@1.8']
+
+    # Add hdf5 onto PATH so we find the 1.8 compiler
+    ENV.prepend_create_path "PATH", hdf5.bin
     system 'make', "sonLibRootPath=#{sonlib.opt_prefix}/sonLib"
 
     bin.install Dir['bin/*']
