92e2660df5f4ac837f963272b7285bc9e575dd84
Matthieu Muffato <muffato@ebi.ac.uk>
2018-02-08 12:52:16 +0000

    Added a fix for GERP

--- gerp.rb
+++ gerp.rb
@@ -14,6 +14,7 @@ class Gerp < Formula
   homepage "http://mendel.stanford.edu/SidowLab/downloads/gerp"
   url "http://mendel.stanford.edu/SidowLab/downloads/gerp/GERPv2.1b_20080211.tar.gz"
   sha256 "0053c47e4ace5ede83959d70ddbffad94e81ccbe6392ac3a4a389e0700e90ce0"
+  revision 1
 
   patch :DATA
   def install
@@ -62,3 +63,19 @@ index 6f67c9b..e3f54ac 100644
  
  #include "Mseq.h"
  #include "MIter.h"
+diff --git a/gerpelem.cc b/gerpelem.cc
+index e3f54ac..dd4c94e 100644
+--- a/gerpelem.cc
++++ b/gerpelem.cc
+@@ -245,6 +245,11 @@ void computeP(Vec<pos> &a, Vec<elem> &found, Vec<elem> sfound[],
+   }
+ 
+   int n = MAX_LEN * SC_RANGE;
++  long true_size = 2 * (long)MAX_LEN * (long)SC_RANGE;
++  if ((2 * n) != true_size) {
++    cerr << "The matrix is too big (" << true_size << " elements) and is causing an integer overflow. Aborting !" << endl;
++    exit(1);
++  }
+   double *p = new double[2 * n];
+   double *gp = new double[2 * n];
+   memset(p, 0, 2 * n * sizeof(double));
