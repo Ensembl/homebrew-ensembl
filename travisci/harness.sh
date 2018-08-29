6034d8b4563947833634fb7342718f6876e5e056
Matthieu Muffato <muffato@ebi.ac.uk>
2018-08-29 10:37:37 +0100

    Need to undo the shallow clone in order to get master

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -6,8 +6,14 @@ set -euo pipefail
 # Enable Ctrl+C if run interactively
 test -t 1 && USE_TTY="-t"
 
-git fetch origin master
-COMMIT_RANGE=${TRAVIS_COMMIT_RANGE:-origin/master..$TRAVIS_BRANCH}
+COMMIT_RANGE="$TRAVIS_COMMIT_RANGE"
+if [ -z "$COMMIT_RANGE" ]
+then
+    # Undo the shallow clone
+    git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+    git fetch --unshallow origin master
+    COMMIT_RANGE="origin/master..$TRAVIS_BRANCH"
+fi
 echo "Testing changed files in $COMMIT_RANGE"
 
 # Test each changed file independently
