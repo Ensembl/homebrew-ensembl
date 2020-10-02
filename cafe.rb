cd9d0ba7077b9b765c5adb46a12d7c6206ea4886
Cristina Guijarro <cristig@ebi.ac.uk>
2020-10-02 14:22:20 +0100

    Include version on previous cafe formula

--- cafe.rb
+++ cafe.rb
@@ -14,7 +14,7 @@ class Cafe < Formula
   homepage "http://sites.bio.indiana.edu/~hahnlab/Software.html"
   url "http://downloads.sourceforge.net/project/cafehahnlab/Previous_versions/cafehahnlab-code_v2.2.tgz"
   sha256 "82853f2df095417708182e4115666162c7cbf67f53778d6a6fa26c1ff58d7473"
-
+  version "2.2"
   # Without "gcc", ld can't find libm and libpthread, e.g.
   # mathfunc.c:(.text+0x5afe): undefined reference to `__log_finite'
   # cafe_main.c:(.text+0x6f08): undefined reference to `pthread_create'
