cbd622122d68e1e629194fb8e1d52e9caf27fa49
Thiago Genez <thiagogenez@ebi.ac.uk>
2020-10-07 11:49:30 +0100

    File linted and auto-corrected via rubocop

--- cactus.rb
+++ cactus.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 
 # Licensed under the Apache License, Version 2.0 (the License);
 # you may not use this file except in compliance with the License.
@@ -13,60 +14,54 @@ require 'fileutils'
 
 class Cactus < Formula
   include Language::Python::Virtualenv
-  
-  desc "Official home of genome aligner based upon notion of Cactus graphs" 
-  homepage "https://github.com/ComparativeGenomicsToolkit/cactus"
-  url "https://github.com/ComparativeGenomicsToolkit/cactus/releases/download/v1.2.3/cactus-v1.2.3.tar.gz"
-  sha256 "49e686f3381e3cf7f7a7696a8506fb1edadc21468f7ac56f8ea49e4d1618c8c2" 
-  head "https://github.com/ComparativeGenomicsToolkit/cactus.git"
+
+  desc 'Official home of genome aligner based upon notion of Cactus graphs'
+  homepage 'https://github.com/ComparativeGenomicsToolkit/cactus'
+  url 'https://github.com/ComparativeGenomicsToolkit/cactus/releases/download/v1.2.3/cactus-v1.2.3.tar.gz'
+  sha256 '49e686f3381e3cf7f7a7696a8506fb1edadc21468f7ac56f8ea49e4d1618c8c2'
+  head 'https://github.com/ComparativeGenomicsToolkit/cactus.git'
 
   bottle :unneeded
 
-
-  depends_on "pkg-config" => :build
-  depends_on "gcc@7" => :build
-  depends_on "hdf5" => "enable-cxx"
-  depends_on "lzo"
-  depends_on "python3"
-
+  depends_on 'pkg-config' => :build
+  depends_on 'gcc@7' => :build
+  depends_on 'hdf5' => 'enable-cxx'
+  depends_on 'lzo'
+  depends_on 'python3'
 
   def install
-  	ENV.deparallelize
-  	
-  	# create virtual environment
-	venv = virtualenv_create(libexec, "python3")
+    ENV.deparallelize
 
-	# fix `pip` command because Cactus has nested-pip calls for sonLib
-	inreplace "setup.py", 
-		"pip", 
-		"#{libexec}/bin/pip"
+    # create virtual environment
+    venv = virtualenv_create(libexec, 'python3')
 
+    # fix `pip` command because Cactus has nested-pip calls for sonLib
+    inreplace 'setup.py',
+              'pip',
+              "#{libexec}/bin/pip"
 
-	#Extracting python dependencies from setup.py because
-	#the pip call Python.rb use --no-dependencies flag
-	extract_python_dependencies(libexec)
-	
+    # Extracting python dependencies from setup.py because
+    # the pip call Python.rb use --no-dependencies flag
+    extract_python_dependencies(libexec)
 
-	# Forcing the installment of cactus python (binary) dependencies without using
-	# invidual package source code (.tar.gz) via `venv.pip_install resources` 
-	system "#{libexec}/bin/pip", 
-		"install",
-		"-r", 
-		"toil-requirement.txt" 
+    # Forcing the installment of cactus python (binary) dependencies without using
+    # invidual package source code (.tar.gz) via `venv.pip_install resources`
+    system "#{libexec}/bin/pip",
+           'install',
+           '-r',
+           'toil-requirement.txt'
 
+    # install cactus python dependencies
+    venv.pip_install_and_link buildpath
 
-	# install cactus python dependencies
-	venv.pip_install_and_link buildpath
+    # Compilation adjustments
+    inreplace 'submodules/sonLib/include.mk',
+              '.h',
+              '.h.inv'
+    ENV['CXX_ABI_DEF'] = '-D_GLIBCXX_USE_CXX11_ABI=1'
 
-	
-	# Compilation adjustments
-    inreplace "submodules/sonLib/include.mk",
-    	".h",
-    	".h.inv"
-    ENV['CXX_ABI_DEF']="-D_GLIBCXX_USE_CXX11_ABI=1"
-    
     # Cactus compilation
-    system "make"
+    system 'make'
 
     # installation
     bin.install Dir['bin/*']
@@ -74,17 +69,15 @@ class Cactus < Formula
     include.install Dir['include/*.h']
     share.install Dir['share/*']
 
-    # create symbolic links for Cactus be able to use toil 
+    # create symbolic links for Cactus be able to use toil
     bin.install_symlink "#{libexec}/bin/_toil_worker"
     bin.install_symlink "#{libexec}/bin/_toil_kubernetes_executor"
     bin.install_symlink "#{libexec}/bin/_toil_mesos_executor"
 
-	#copy python script from submodules to be included at PYTHONPATH
-	path=%x(#{libexec}/bin/python3 -c \"import sysconfig; print(sysconfig.get_paths()['purelib'])\").strip
-    FileUtils.cp_r "submodules/hal",
-    	path
- 	
-
+    # copy python script from submodules to be included at VirtualEnv's lib
+    path = `#{libexec}/bin/python3 -c \"import sysconfig; print(sysconfig.get_paths()['purelib'])\"`.strip
+    FileUtils.cp_r 'submodules/hal',
+                   path
   end
 
   private def extract_python_dependencies(libexec)
@@ -93,24 +86,24 @@ class Cactus < Formula
     f = open("toil-requirement.txt","a");
     f.write("\n".join(result.install_requires));
     f.close();'
-    File.write('toil-requirement.py', wrapper.split(/\s*;\s*/).join("\n"), mode: "a")
+    File.write('toil-requirement.py', wrapper.split(/\s*;\s*/).join("\n"), mode: 'a')
     system "#{libexec}/bin/python3",
-    	"toil-requirement.py"
+           'toil-requirement.py'
   end
 
   def caveats
-    <<~EOS
-     	Cactus Python scripts are already configured to use Cactus' own virtualenv.
-     	If you need to use it, do this:
+    <<-EOS
+      Cactus Python scripts are already configured to use Cactus' own virtualenv.
+      If you need to use it, do this:
 
-     	source #{libexec}/bin/activate
-     	python3 -c "import hal"
-     	deactivate
-
+      source #{libexec}/bin/activate
+      python3 -c "import hal"
+      deactivate
+      
     EOS
   end
 
   test do
-    system "#{bin}/cactus", "--help"
+    system "#{bin}/cactus", '--help'
   end
 end
